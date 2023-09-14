b53562b99ff705800f7402748475589378b75e72
Roberto <mirandaroberto1994@gmail.com>
2023-09-14 15:56:26 -0300

    agrega listado de servidores dinamicamente

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
-console.log('script.js');
+function loadData() {
+    fetch('http://127.0.0.1:5000/servidor/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Manipula los datos y muestra en la página
+            var dataList = document.getElementById('data-list');
+            
+            // Limpia los datos anteriores
+            dataList.innerHTML = '';
+
+            // Itera sobre los datos y crea elementos <li> para cada elemento
+            data.forEach(item => {
+                var listItem = document.createElement('li');
+                listItem.textContent = item[1]; // Accede al segundo elemento (en este caso, 'prueba')
+                dataList.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+// Llama a la función para cargar datos cuando se cargue la página
+loadData();
