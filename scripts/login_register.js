a4f29aa8d746c383233bdad2e882b514817fdcb3
Roberto <mirandaroberto1994@gmail.com>
2023-09-24 13:19:38 -0300

    agrega cerrar sesion

--- scripts/login_register.js
+++ scripts/login_register.js
@@ -73,3 +73,26 @@ function register() {
             console.error("Error:", error);
         });
 }
+
+function logout() {
+    const url = "http://127.0.0.1:5000/usuario/cerrar_sesion";
+
+    fetch(url, {
+        method: "GET",
+        credentials: "include",
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json().then((data) => {
+                    window.location.href = "login.html";
+                });
+            } else {
+                return response.json().then((data) => {
+                    document.getElementById("message").innerHTML = data.message;
+                });
+            }
+        })
+        .catch((error) => {
+            document.getElementById("message").innerHTML = "Ocurrió un error.";
+        });
+}
